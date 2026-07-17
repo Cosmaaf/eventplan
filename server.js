ea4e6a05d32e2611db3f37f955ad3f2c73cf88cd
Илья <ilya@example.com>
2026-07-17 16:20:31 +0300

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const bot = new TelegramBot(token, { polling: true });
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
